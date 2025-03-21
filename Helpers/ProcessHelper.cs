985c6132b4d9113f3508bca8ec162ee2b1c0a975
BenSabry <BenSabry@outlook.com>
2025-03-21 23:47:30 +0200

    fixes

--- Helpers/ProcessHelper.cs
+++ Helpers/ProcessHelper.cs
@@ -4,32 +4,31 @@ namespace Shared
 {
     public static class ProcessHelper
     {
-        const string NoArgs = "";
+        public static void RunShell(string path, string? args = default) => RunAsync(path, args, true, false).Wait();
+        public static async Task RunShellAsync(string path, string? args = default) => await RunAsync(path, args, true, false);
 
-        public static void RunShell(string path, string args = NoArgs) => RunAsync(path, args, true, false).Wait();
-        public static async Task RunShellAsync(string path, string args = NoArgs) => await RunAsync(path, args, true, false);
+        public static void Run(string path, string? args = default, bool useShell = false) => RunAsync(path, args, useShell, false).Wait();
+        public static async Task RunAsync(string path, string? args = default, bool useShell = false) => await RunAsync(path, args, useShell, false);
 
-        public static void Run(string path, string args = NoArgs, bool useShell = false) => RunAsync(path, args, useShell, false).Wait();
-        public static async Task RunAsync(string path, string args = NoArgs, bool useShell = false) => await RunAsync(path, args, useShell, false);
+        public static string RunAndReadOutput(string path, string? args = default) => RunAsync(path, args, false, true).GetAwaiter().GetResult();
+        public static async Task<string> RunAndReadOutputAsync(string path, string? args = default) => await RunAsync(path, args, false, true);
 
-        public static string RunAndReadOutput(string path, string args = NoArgs) => RunAsync(path, args, false, true).GetAwaiter().GetResult();
-        public static async Task<string> RunAndReadOutputAsync(string path, string args = NoArgs) => await RunAsync(path, args, false, true);
-
-        private static async Task<string> RunAsync(string path, string args, bool useShell, bool readOutput)
+        private static async Task<string> RunAsync(string path, string? args, bool useShell, bool readOutput)
         {
+            var redirectOutput = !useShell && readOutput;
             var process = Process.Start(new ProcessStartInfo
             {
                 FileName = path,
                 Arguments = args,
                 UseShellExecute = useShell,
-                RedirectStandardOutput = !useShell,
+                RedirectStandardOutput = redirectOutput,
                 CreateNoWindow = !useShell
             });
 
             if (process is null || process.HasExited)
                 throw new InvalidOperationException($"Failed to run {path} {args}");
 
-            if (readOutput && !useShell)
+            if (redirectOutput)
             {
                 var output = await process
                     .StandardOutput.ReadToEndAsync();
